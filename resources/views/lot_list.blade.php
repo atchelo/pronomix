ece45c8720914bcc6d2231fd0127afd6252c98b8
atchelo <atchelokonand1@gmail.com>
2022-07-10 07:33:03 +0000

    first part

--- resources/views/lot_list.blade.php
+++ resources/views/lot_list.blade.php
@@ -186,7 +186,7 @@
     <!-- * App Capsule -->
 </div>
 
-
+@include('components.toast')
 
 <!-- Add Card Action Sheet -->
 <div class="modal fade action-sheet" id="addCardActionSheet" tabindex="-1" role="dialog">
@@ -296,17 +296,6 @@
 </div>
 <!-- * DialogIconedSuccess -->
 
-<div id="toast-7" class="toast-box toast-bottom" style="justify-content: center">
-    <div class="in">
-        <ion-icon name="document-outline" style="width: 24px"></ion-icon>
-        <ion-icon name="trash-outline" style="width: 24px" data-bs-toggle="modal" data-bs-target="#DialogIconedButtonInline1"></ion-icon>
-    </div>
-    <div class="in" style="padding: 0">
-        <button type="button" id="coup_pron" class="btn btn-secondary" style="border-radius: inherit; background: white !important; border:white !important; color: #11a44c !important;">PRONOSTIC MULTIPLE(<span id="pron_numb"></span>)</button>
-        {{---<ion-icon class="close-button" name="close-circle-outline" style="width: 24px"></ion-icon>---}}
-    </div>
-</div>
-
 
 <!-- Dialog Iconed Inline -->
 <div class="modal fade dialogbox" id="DialogIconedButtonInline1" data-bs-backdrop="static" tabindex="-1"
@@ -347,66 +336,6 @@
 <script src="assets/js/base.js"></script>
 
 <script>
-
-    $('#coup_pron').click(function(e) {
-        $("#loader").show();
-        var p = new Object();
-
-        p['token'] = "{{$token}}";
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            type: "GET",
-            url: `https://demo.pronomix.net/api/coupon-pronostics`,
-            data: p,
-            success: function(data) {
-                if (data.success === true){
-                    console.log(data)
-                    var new_token = data.new_token;
-                    var data_reg = data.data;
-                    var o = new Object();
-                    o["new_token"] = new_token;
-                    o["data_reg"] = data_reg;
-                    var url = "{{ route('pronos_multi') }}";
-                    //window.location = `${url}?new_token=` + new_token + `&match_data=` + match_data;
-
-                    $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        }
-                    });
-
-                    $.ajax({
-                        type: "POST",
-                        url: url,
-                        data: o,
-                        success: function(data) {
-                            window.location = "{{ route('coup_pron') }}";
-                        },
-                        statusCode: {
-                            500: function() {
-                                $('#coup_error').append("Une erreur est survemue. Merci de ressayer plutard.");
-                                $("#loader").hide();
-                                $('#DialogIconedDanger').modal('show');
-                            }
-                        }
-                    });
-                }
-
-            },
-            statusCode: {
-                500: function() {
-                    $('#coup_error').append("Une erreur est survemue. Merci de ressayer plutard.");
-                    $("#loader").hide();
-                    $('#DialogIconedDanger').modal('show');
-                }
-            }
-        });
-
-    });
 
     window.addEventListener("load", function() {
 
